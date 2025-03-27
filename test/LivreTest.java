a0e094bbf5be98c62db7f8b1c35a3da147ded815
yanislitim <yanis.litim@etu.univ-orleans.fr>
2025-03-27 10:41:04 +0100

    correction des test

--- test/LivreTest.java
+++ test/LivreTest.java
@@ -1,21 +1,21 @@
+import junit.*;
+
 public class LivreTest {
-@Test
-    public void TestGetIsbn(){
-        Livre Lelivredelajungle = new Livre(1,"Le livre de la jungle","281","01/01/1999",19);
-        assert Lelivredelajungle.getIsbn() == 1;
-        assert Lelivredelajungle.getTitre() == "Le livre de la jungle";
-    }
+        @Test
+        public void TestGetIsbn(){
+            Livre Lelivredelajungle = new Livre(1,"Le livre de la jungle","281","01/01/1999",19);
+            assert Lelivredelajungle.getIsbn() == 1;
+            assert Lelivredelajungle.getTitre() == "Le livre de la jungle";
+        }
+
+
+
+        @Test
+        public void TestGetTitre(){
+            Livre Lelivredelajungle = new Livre(1,"Le livre de la jungle","281","01/01/1999",19);
+            assert Lelivredelajungle.getTitre() == "Le livre de la jungle";
+        }
 }
-
-
-@Test
-    public void TestGetTitre(){
-        Livre Lelivredelajungle = new Livre(1,"Le livre de la jungle","281","01/01/1999",19);
-        assert Lelivredelajungle.getTitre() == "Le livre de la jungle";
-    }
-
-
-
 
 
 
