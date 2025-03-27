b35a9bb59dd666656620a0df55548043521a64e3
Arthur <arthur.santenac@etu.univ-orleans.fr>
2025-03-27 10:13:24 +0100

    ajout des fichier Magasin, Personne, Vendeur

---
+++ src/Magasin.java
@@ -0,0 +1,37 @@
+public class Magasin {
+    
+    private int idMag;
+    private String nomMag;
+    private String villeMag;
+
+    public Magasin(int idMag, String nomMag, String villeMag) {
+        this.idMag = idMag;
+        this.nomMag = nomMag;
+        this.villeMag = villeMag;
+    }
+
+    public int getIdMag() {
+        return idMag;
+    }
+
+    public void setIdMag(int idMag) {
+        this.idMag = idMag;
+    }
+
+    public String getNomMag() {
+        return nomMag;
+    }
+
+    public void setNomMag(String nomMag) {
+        this.nomMag = nomMag;
+    }
+
+    public String getVilleMag() {
+        return villeMag;
+    }
+
+    public void setVilleMag(String villeMag) {
+        this.villeMag = villeMag;
+    }
+
+}
---
+++ src/Vendeur.java
@@ -0,0 +1,18 @@
+public class Vendeur extends Personne{
+    
+    private int idVendeur;
+
+    public Vendeur(String nom, String prenom, String adresse, String codePostal, String ville, int idVendeur) {
+        super(nom, prenom, adresse, codePostal, ville);
+        this.idVendeur = idVendeur;
+    }
+
+    public int getIdVendeur() {
+        return idVendeur;
+    }
+
+    public void setIdVendeur(int idVendeur) {
+        this.idVendeur = idVendeur;
+    }
+
+}
