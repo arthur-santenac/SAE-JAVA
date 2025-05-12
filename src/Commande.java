320cf5c0c45a14387e401b49ef1e2e5c4cc5f00c
Louis-2503 <louis.leray@etu.univ-orleans.fr>
2025-05-12 11:26:33 +0200

    Constructeur et getters de commande.java

--- src/Commande.java
+++ src/Commande.java
@@ -1,3 +1,30 @@
 public class Commande {
-    
+
+    private int numCom;
+    private String dateCom;
+    private String enLigne;
+    private String livraison;
+
+    public Commande(int numCom, String dateCom, String enLigne, String livraison){
+        this.numCom = numCom;
+        this.dateCom = dateCom;
+        this.enLigne = enLigne;
+        this.livraison = livraison;
+    }
+
+    public int getNumCom() {
+        return this.numCom;
+    }
+
+    public String getDateCom() {
+        return this.dateCom;
+    }
+
+    public String getCommande() {
+        return this.enLigne;
+    }
+
+    public String getLivraison() {
+        return this.livraison;
+    }
 }
