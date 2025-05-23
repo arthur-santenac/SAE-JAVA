3c7bc07823885dd4a53a8c2d39810d27193c1dda
yanislitim <yanis.litim@etu.univ-orleans.fr>
2025-05-23 11:54:07 +0200

    ajout classe commande et detail commande

---
+++ src/DetailCommande.java
@@ -0,0 +1,23 @@
+public class DetailCommande {
+    private int numLig;
+    private int qte;
+    private double prixVente;
+
+    public DetailCommande(int numLig, int qte, double prixVente) {
+        this.numLig = numLig;
+        this.qte = qte;
+        this.prixVente = prixVente;
+    }
+
+    public int getNumLig() {
+        return numLig;
+    }
+
+    public int getQte() {
+        return qte;
+    }
+
+    public double getPrixVente() {
+        return prixVente;
+    }
+}
