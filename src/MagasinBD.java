2ed5d84e388944996debbba5e5a82800d2a0e55b
De Oliveira Hugo <o22303787@or-iut-i201-f16.iut45.univ-orleans.fr>
2025-05-27 08:56:34 +0200

    Ajout de la fonction listeDesMagasins dans MagasinBD

--- src/MagasinBD.java
+++ src/MagasinBD.java
@@ -1,5 +1,6 @@
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MagasinBD {
     
@@ -15,7 +16,18 @@ public class MagasinBD {
     }
 
     ArrayList<Magasin> listeDesMagasins() throws SQLException{
-		throw new SQLException("méthode listeDesJoueurs à implémenter");
+		this.st = this.laConnexion.createStatement();
+		ResultSet rs = this.st.executeQuery("Select * from MAGASIN");
+		List<Magasin> magasins = new ArrayList<>();
+		while(rs.next()){
+			int id = rs.getInt(1);
+			String nomM = rs.getString(2);
+			String villeM = rs.getString(3);
+			Magasin m = new Magasin(id,nomM, villeM);
+			magasins.add(m);
+		}
+		rs.close();
+		return (ArrayList<Magasin>) magasins;
 	}
 
     ArrayList<String> listeDesNomDeMags() throws SQLException{
