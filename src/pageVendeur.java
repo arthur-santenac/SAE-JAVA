4f0db364a78c6873f7b6c622cd15a2e27ab32706
Louis-2503 <louis.leray@etu.univ-orleans.fr>
2025-06-16 15:26:11 +0200

    vue vendeur

---
+++ src/pageVendeur.java
@@ -0,0 +1,73 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javax.swing.plaf.synth.Region;
+
+public class pageVendeur extends BorderPane {
+
+    public pageVendeur() {
+
+        super();
+
+        this.setTop(this.top());
+        this.setLeft(this.left());
+        this.setCenter(this.center());
+        this.setRight(this.right());
+
+    }
+
+    public HBox top() {
+        Label titre = new Label("Livre Express");
+        Button btnDeconnexion = new Button("deconnexion");
+        HBox top = new HBox(20, titre, btnDeconnexion);
+        top.setStyle("-fx-padding: 10; -fx-alignment: center_left; -fx-background-color: #666; -fx-spacing: 20;");
+        titre.setStyle("-fx-text-fill: white; -fx-font-size: 20px;");
+        btnDeconnexion.setStyle("-fx-background-color: #ddd;");
+        return top;
+    }
+
+    public VBox left() {
+        Label bonjour = new Label("Bonjour *nom vendeur* !");
+        Button btnAjout = new Button("ajouter un livre à la librairie");
+        Button btnStock = new Button("modifier les stocks d’un livre");
+        Button btnTransfert = new Button("transférer un livre d’une autre librairie");
+        VBox left = new VBox(10, bonjour, btnAjout, btnStock, btnTransfert);
+        left.setStyle("-fx-padding: 10;");
+        return left;
+    }
+
+    public VBox center() {
+        VBox center = new VBox();
+        return center;
+    }
+
+    public VBox right() {
+        TextField champId = new TextField();
+        champId.setPromptText("id de l’article à ajouter");
+        champId.setPrefWidth(200);
+
+        Button btnAjouterArticle = new Button("ajouter");
+        HBox ajoutZone = new HBox(10, champId, btnAjouterArticle);
+        ajoutZone.setAlignment(Pos.CENTER_RIGHT);
+
+        Label resumeTitre = new Label("Résumé de la commande");
+        resumeTitre.setStyle("-fx-background-color: black; -fx-text-fill: white; -fx-padding: 5;");
+        ListView<String> listeCommande = new ListView<>();
+
+        VBox resumeBox = new VBox(resumeTitre, listeCommande);
+        resumeBox.setStyle("-fx-border-color: black;");
+        resumeBox.setPrefSize(300, 400);
+
+        Button btnFinaliser = new Button("finaliser commande");
+        btnFinaliser.setStyle("-fx-background-color: #666; -fx-text-fill: white;");
+
+        VBox rightPanel = new VBox(15, ajoutZone, resumeBox, btnFinaliser);
+        rightPanel.setPadding(new Insets(20));
+        rightPanel.setAlignment(Pos.TOP_RIGHT);
+        return rightPanel;
+    }
+}
