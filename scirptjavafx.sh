00fafd08ec54856197e58513584978074f183913
Arthur <arthur.santenac@etu.univ-orleans.fr>
2025-06-17 10:33:37 +0200

    ajout connexionBD (marche pas encore)

--- scirptjavafx.sh
+++ scirptjavafx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 javac -d bin --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls src/*.java
-java -cp bin:img --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls LivreExpress
+java -cp bin:img:lib/mariadb-java-client-3.5.3.jar --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls LivreExpress
--- src/ControleurConnexion.java
+++ src/ControleurConnexion.java
@@ -5,16 +5,17 @@ import java.sql.Statement;
 import org.w3c.dom.events.Event;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class ControleurConnexion implements EventHandler<ActionEvent> {
 
     private LivreExpress appli;
-    private String email;
-    private String mdp;
+    private TextField email;
+    private TextField mdp;
     private Statement st;
     private ConnexionMySQL laConnexion;
 
-    public ControleurConnexion(ConnexionMySQL laConnexion, LivreExpress appli, String email, String mdp) {
+    public ControleurConnexion(ConnexionMySQL laConnexion, LivreExpress appli, TextField email, TextField mdp) {
         this.laConnexion = laConnexion;
         this.appli = appli;
         this.email = email;
@@ -26,10 +27,11 @@ public class ControleurConnexion implements EventHandler<ActionEvent> {
         Button button = (Button) (event.getSource());
         if (laConnexion == null) {
             try {
-                appli.setConnexion(email, mdp, "servinfo-maria", "DB" + email);
+                this.appli.setConnexion(email.getText(), mdp.getText(), "servinfo-maria", "DB" + email.getText());
+                this.appli.afficheConnexion();
             } catch (SQLException e) {
 
-            }
+            } 
         } else if (button.getText().equals("Déconnexion")) {
             this.appli.afficheConnexion();
         } else {
@@ -37,7 +39,7 @@ public class ControleurConnexion implements EventHandler<ActionEvent> {
                 st = laConnexion.createStatement();
                 ResultSet set = st.executeQuery("select * from CONNEXION natural join CLIENT");
                 while (set.next()) {
-                    if (email.equals(set.getString(2)) && mdp.equals(set.getString(3))) {
+                    if (email.getText().equals(set.getString(2)) && mdp.getText().equals(set.getString(3))) {
                         appli.setUtilisateur(new Client(set.getString(5), set.getString(6), set.getString(7), set.getString(8), set.getString(9), set.getInt(1)));
                         String compte = set.getString(4);
                         if (compte.equals("client")) {
